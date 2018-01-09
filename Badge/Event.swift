4b7071ac7ba18313595e84fc53c4697fbe853b0a
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2018-01-09 19:32:37 +0900

    Create models

---
+++ Badge/Event.swift
@@ -0,0 +1,13 @@
+//
+//  Event.swift
+//  Badge
+//
+//  Created by mitsuyoshi.yamazaki on 2018/01/09.
+//  Copyright © 2018年 Mitsuyoshi. All rights reserved.
+//
+
+import Foundation
+
+struct Event {
+  let description: String?
+}
