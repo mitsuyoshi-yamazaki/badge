461680822db29051a080296ca589fd430b678810
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2018-01-09 18:52:24 +0900

    Add BadgeListViewController

---
+++ Badge/BadgeListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BadgeListViewController.swift
+//  Badge
+//
+//  Created by mitsuyoshi.yamazaki on 2018/01/09.
+//  Copyright © 2018年 Mitsuyoshi. All rights reserved.
+//
+
+import UIKit
+
+class BadgeListViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
