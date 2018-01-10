e786930e1e62d1df70263a9e73070faeb18605f8
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2018-01-10 18:59:59 +0900

    Add NewAchievementViewController

---
+++ Badge/NewAchievementViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewAchievementViewController.swift
+//  Badge
+//
+//  Created by mitsuyoshi.yamazaki on 2018/01/10.
+//  Copyright © 2018年 Mitsuyoshi. All rights reserved.
+//
+
+import UIKit
+
+final class NewAchievementViewController: UITableViewController {
+    
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
---
+++ Badge/UIViewControllerExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewControllerExtensions.swift
+//  Badge
+//
+//  Created by mitsuyoshi.yamazaki on 2018/01/10.
+//  Copyright © 2018年 Mitsuyoshi. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+  @IBAction func dismiss(sender: AnyObject) {
+    dismiss(animated: true, completion: nil)
+  }
+}
