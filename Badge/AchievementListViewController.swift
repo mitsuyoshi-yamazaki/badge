ff7e67ce1c40e8129e557285d5848ae96d6b10e0
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2018-01-17 19:08:41 +0900

    Add share menu for exporting all data for debugging

--- Badge/AchievementListViewController.swift
+++ Badge/AchievementListViewController.swift
@@ -20,6 +20,7 @@ final class AchievementListViewController: UIViewController {
   
   @IBOutlet private weak var collectionView: UICollectionView!
   @IBOutlet private weak var addButton: UIBarButtonItem!
+  @IBOutlet private weak var exportButton: UIBarButtonItem!
 
   private var achievements: [Achievement] = [] {
     didSet {
@@ -36,24 +37,54 @@ final class AchievementListViewController: UIViewController {
     loadAchievements()
     
     addButton.rx.tap.asDriver()
-      .drive(onNext: { [weak self] _ in
-        let newAchievementViewController = NewAchievementViewController.instantiate(with: .init())
-        
-        newAchievementViewController.newAchievementDriver
-          .drive(onNext: { [weak self] newAchievement in
-            self?.achievements.append(newAchievement)
-            self?.collectionView.reloadData()
-          })
-          .disposed(by: newAchievementViewController.disposeBag)
-        
-        let navigationController = UINavigationController.init(rootViewController: newAchievementViewController)
-        
-        self?.present(navigationController, animated: true, completion: nil)
-      })
+      .map { _ in }
+      .drive(presentNewAchievementView)
+      .disposed(by: disposeBag)
+    
+    exportButton.rx.tap.asDriver()
+      .map { [weak self] _ -> [Achievement]? in self?.achievements }
+      .filterNil()
+      .drive(debugShareData)
       .disposed(by: disposeBag)
   }
 }
 
+// MARK: - Behavior
+private extension AchievementListViewController {
+  var presentNewAchievementView: Binder<Void> {
+    return Binder(self) { viewController, _ in
+      let newAchievementViewController = NewAchievementViewController.instantiate(with: .init())
+
+      newAchievementViewController.newAchievementDriver
+        .drive(onNext: { [weak viewController] newAchievement in
+          viewController?.achievements.append(newAchievement)
+          viewController?.collectionView.reloadData()
+        })
+        .disposed(by: newAchievementViewController.disposeBag)
+      
+      let navigationController = UINavigationController.init(rootViewController: newAchievementViewController)
+      
+      viewController.present(navigationController, animated: true, completion: nil)
+    }
+  }
+  
+  var debugShareData: Binder<[Achievement]> {
+    return Binder(self) { viewController, achievements in
+      guard
+        let jsonData = try? JSONEncoder().encode(achievements),
+        let dataString = String.init(data: jsonData, encoding: String.Encoding.utf8)
+      else {
+        return
+      }
+      let items = [dataString]
+      let activityController = UIActivityViewController.init(activityItems: items, applicationActivities: nil)
+      
+      viewController.present(activityController, animated: true, completion: nil)
+    }
+  }
+}
+
+// MARK: - Store Data
 private extension AchievementListViewController {
   var userDefaultsKey: String {
     let bundleID = Bundle.main.bundleIdentifier!
@@ -78,6 +109,7 @@ private extension AchievementListViewController {
   }
 }
 
+// MARK: - UICollectionViewDataSource
 extension AchievementListViewController: UICollectionViewDataSource {
   func numberOfSections(in collectionView: UICollectionView) -> Int {
     return 1
@@ -97,5 +129,6 @@ extension AchievementListViewController: UICollectionViewDataSource {
   }
 }
 
+// MARK: - UICollectionViewDelegate
 extension AchievementListViewController: UICollectionViewDelegate {
 }
